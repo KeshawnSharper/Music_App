292a29fceb46bfd4fa5f1f973051a9659d894281
Lambda_School_Loaner_182 <ksharper@studentmba.org>
2021-04-01 13:59:23 -0400

    Can make api calls

--- server.go
+++ server.go
@@ -1,11 +1,21 @@
 package main
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"gitlab.com/pragmaticreviews/golang-gin-poc/controller"
+	"gitlab.com/pragmaticreviews/golang-gin-poc/service"
+)
+var(
+	songService    service.VideoService       = service.New()
+	songController controller.VideoController = controller.New(songService)
+
+)
 func main(){
 	server := gin.Default()
-	server.GET("/test",func(ctx *gin.Context){
-		ctx.JSON(200,gin.H{
-			"message":"OK!!",
-		})
+	server.GET("/songs",func(ctx *gin.Context){
+		ctx.JSON(200,songController.FindAll())
+	})
+	server.POST("/songs",func(ctx *gin.Context){
+		ctx.JSON(200,songController.Save(ctx))
 	})
 server.Run()
 }

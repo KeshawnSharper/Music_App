48fabf43a58d8abb078cdb2a82cc8b805884b96b
Keshawn Sharper <Ksharper@studentmba.org>
2021-04-03 21:21:20 -0400

    save

--- server.go
+++ server.go
@@ -1,8 +1,9 @@
 package main
 import (
 	"io"
-	"o"
-	"github.com/gin-gonic/gin
+	"os"
+	
+	"github.com/gin-gonic/gin"
 	"gitlab.com/pragmaticreviews/golang-gin-poc/middlewares"
 	"gitlab.com/pragmaticreviews/golang-gin-poc/controller"
 	"gitlab.com/pragmaticreviews/golang-gin-poc/service"

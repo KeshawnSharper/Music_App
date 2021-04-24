9a0776a1113d42d83340c070b4844c351dce6b33
KeshawnSharper <ksharper@studentmba.org>
2021-04-24 06:09:59 -0400

    Deleted past go files

--- entity/song.go
+++
@@ -1,5 +0,0 @@
-package entity
-type Song struct{
-	Title string `json:"title"`
-	Description string`json:"description"`
-}
--- middlewares/basic-auth.go
+++
@@ -1,7 +0,0 @@
-package middlewares
-import 
-func BasicAuth() gin.HandlerFunc {
-return gin.BasicAuth(gin.Accounts{
-	"pragmatic" ; "reviews"
-})
-}

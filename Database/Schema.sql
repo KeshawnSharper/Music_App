9033bca41810897e7c375e3301db4592026c4e55
KeshawnSharper <ksharper@studentmba.org>
2021-04-24 06:23:28 -0400

    created users_table

--- Database/Schema.sql
+++ Database/Schema.sql
@@ -1,4 +1,9 @@
 CREATE TABLE base_table{
-    created_at TIMESTAMP NOT NULL
+    created_at TIMESTAMP NOT NULL,
     updated at TIMESTAMP NOT NULL
 }
+CREATE TABLE user_account{
+    id uuid PRIMARY KEY uuid_generate_v1(),
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL
+} INHERITS (base_table)

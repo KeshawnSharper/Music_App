436b8941ba3c225397e3723201e34232c4cf286f
KeshawnSharper <ksharper@studentmba.org>
2021-04-24 08:16:12 -0400

    created items table

--- Database/Schema.sql
+++ Database/Schema.sql
@@ -7,3 +7,11 @@ CREATE TABLE user_account{
     email VARCHAR(255) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL
 } INHERITS (base_table)
+CREATE TABLE item{
+    id uuid PRIMARY KEY uuid_generate_v1(),
+    title VARCHAR(255) NOT NULL,
+    notes text,
+    seller_id uuid,
+    price_in_cents INTEGER,
+    FOREIGN KEY(seller_id) REFERENCES user_account(id) ON DELETE CASCADE
+} INHERITS (base_table)
